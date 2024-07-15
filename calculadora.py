6eff1c6492f570ac82460f3bbedcaa8870ff47c0
IcaroMackey <icaromackey@hotmail.com>
2024-07-15 00:42:00 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+x = int(input('Valor 1: '))
+y = int(input('Valor 2: '))
+
+soma = x+y
+subt = x-y
+divs = x//y
+mult = x*y
+
+print('O valor da soma é: '+ str(soma))
+print('O valor da subtração é: '+ str(subt))
+print('O valor da divisão é: '+ str(divs))
+print('O valor da multipicação é: '+ str(mult))
